bf29c13121a8210f3590b12b950306372a0fdfec
iaros <iaroslav-stashevsky@yandex.ru>
2023-05-23 23:23:50 +0300

    version 1.0.5 --добавлен урок 07.04.2023 (смотреть папку uroki, все классы с приставкой 07.04.23) --добавлен урок 17.04.2023 (исключения try/catch, throws) --коллекции --HotelWorks done --Доделаны задачи по ArrayList

--- src/precticeWithArray/HomeWork2605.java
+++ src/precticeWithArray/HomeWork2605.java
@@ -1,17 +1,15 @@
 package precticeWithArray;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-
-import hotelWorks.HotelWorks;
 import hotelWorksClient.Client;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class HomeWork2605 {
     public static void main(String[] args) {
-//        task1();
+        task1();
 //        task2();
-        task3();
+//        task3();
     }
 
     static void task1() {
@@ -48,25 +46,20 @@ public class HomeWork2605 {
         array1.remove(3);
 //        Вывести размер списка
         System.out.println("Размер массива: " + array1.size());
-        System.out.println(array1.get(0) + " " + array1.get(1) + " " + array1.get(2) + " " + array1.get(3) + " " +
-                array1.get(4) + " " + array1.get(5));
 //        Отсортировать список по возрастанию элементов 2 способами: используя Collections.sort(list)(изучить), и использую цикл.
 //        Collections.sort(array1);
-//        System.out.println("Отсортированный массив" + array1);
-//
+//ДОДЕЛАТЬ! 0 12 2 54 3
         for (int j = 0; j < array1.size() - 1; j++) {
-            if (array1.get(j) > array1.get(j + 1)) {
-                Collections.swap(array1, j, j + 1);
-            }
-        }
-        for (int k = 0; k < array1.size() - 1; k++) {
-            if (array1.get(k) > array1.get(k + 1)) {
-                Collections.swap(array1, k, k + 1);
+            for (int k =j + 1; k < array1.size(); k++){
+                if (array1.get(j) > array1.get(k)) {
+                    int x = array1.get(j);
+                    array1.set(j, array1.get(k));
+                    array1.set(k, x);
+                }
             }
         }
         System.out.println("Отсортированный массив" + array1);
     }
-
     static void task2() {
 //        Создать список из 5 строк, в которых указаны названия фруктов.
         ArrayList<String> fruits = new ArrayList<>();
@@ -87,7 +80,7 @@ public class HomeWork2605 {
         vegetables.add("Гранд Чеснокко");
         vegetables.add("Фу Перец-болгарин");
 //        Добавить все элементы списка овощей в список фруктов с 1 позиции.
-        fruits.addAll(0, vegetables);
+        fruits.addAll(1, vegetables);
 //        Вывести список на экран
         System.out.println("Добавили овощей: " + fruits);
 //        Создать копию списка используя clone.
@@ -111,7 +104,12 @@ public class HomeWork2605 {
         clients1.add(new Client("Алексей", "Пятница", 66));
         clients1.add(new Client("Алёна", "Дэнс", 18));
 //        Вывести на экран индекс клиента Алексея.
-        System.out.println("Индекс клиента Алексея: " + clients1.indexOf(new Client("Алексей", "Пятница", 66)));
+        for (Client client : clients1){
+            if (client.getName().equals("Алексей")){
+                System.out.println("Индекс клиента Алексея: " + clients1.indexOf(client));
+            }
+        }
+//        System.out.println("Индекс клиента Алексея: " + clients1.indexOf(new Client("Алексей", "Пятница", 66)));
 //        Вывести на экран элемент, находящийся на позиции 2.
         System.out.println("Клиент на позиции 2: " + clients1.get(2));
 //        Добавить ещё одного клиента «Федор»
@@ -127,16 +125,19 @@ public class HomeWork2605 {
 //        Вывести список1 на экран. В полном виде.
         System.out.println("Список 1 в полном виде: " + clients1);
 //        Вывести список1 на экран, только имена клиентов.
-        System.out.println("Имена клиентов: " + clients1.get(0).getName() + "; " + clients1.get(1).getName() +
-                "; " + clients1.get(2).getName() + "; " + clients1.get(3).getName() + "; " + clients1.get(4).getName() +
-                "; " + clients1.get(5).getName() + "; " + clients1.get(6).getName() + ";");
+        System.out.print("Имена клиентов: ");
+        for (Client client : clients1) {
+            System.out.print(client.getName() + "; ");
+        }
+        System.out.println("\n");
 //        Удалить список2 из списка1.
         clients1.removeAll(clients2);
 //        Вывести на экран список1
         System.out.println("Список 1 после удаления: " + clients1);
 //        Заменить элемент на позиции 1 на клиента (Юля). (set)
-        clients1.set(0,new Client("Юля", "Пуля", 41));
+        clients1.set(0, new Client("Юля", "Пуля", 41));
 //        Вывести на экран
         System.out.println("Инфа о клиенте на позиции 1: " + clients1.get(0));
+        System.out.println(clients1);
     }
 }
